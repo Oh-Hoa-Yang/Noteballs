9ce5302c8b2101f078d403d5406873b65732875a
Oh Hoa Yang <thunderohy@Ohs-MacBook-Air.local>
2025-12-01 14:28:12 +0800

    update: firebase installation setup

---
+++ src/js/firebase.js
@@ -0,0 +1,20 @@
+// All comments are removed (copy & paste from the firebase setup instructions)
+
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCPoRe71VzK_j7ufB6453BdhS5O8w60nlQ',
+  authDomain: 'noteballs-50a2a.firebaseapp.com',
+  projectId: 'noteballs-50a2a',
+  storageBucket: 'noteballs-50a2a.firebasestorage.app',
+  messagingSenderId: '58516923416',
+  appId: '1:58516923416:web:1cf0916588475ec3d48f6d'
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export {
+    db
+}
