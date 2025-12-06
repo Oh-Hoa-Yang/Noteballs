280e33ea3835d97d531011ad9d98674e50642230
Oh Hoa Yang <thunderohy@Ohs-MacBook-Air.local>
2025-12-06 22:30:02 +0800

    update: show placeholder when no notes

--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -55,6 +55,13 @@
                 :note="note"
                 />
                 <!-- @deleteClicked="deleteNote" -->
+
+                <div 
+                    v-if="!storeNotes.notes.length"
+                    class="is-size-4 has-text-centered has-text-grey-light is-family-monospace py-6"
+                >
+                    No notes here yet...
+                </div>
         </template>
 
 
