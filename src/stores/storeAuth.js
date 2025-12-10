33801d8eb93a9264941224bbdd3ead46556f104f
Oh Hoa Yang <thunderohy@Ohs-MacBook-Air.local>
2025-12-10 23:35:41 +0800

    update: Redirect User on Auth Change

--- src/stores/storeAuth.js
+++ src/stores/storeAuth.js
@@ -17,9 +17,11 @@ export const useStoreAuth = defineStore('storeAuth', {
                 // console.log('User logged in: ', user)
                 this.user.id = user.uid
                 this.user.email = user.email
+                this.router.push('/')
             } else {
                 // console.log('User logged out: ', user)
                 this.user = {}
+                this.router.replace('/auth')
             }
         })
     },
